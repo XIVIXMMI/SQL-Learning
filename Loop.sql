86aacfd5f1221be2fa5462a6e3bddb00f3145eec
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-10-12 22:20:02 +0700

    Loop

--- Loop.sql
+++ Loop.sql
@@ -1,8 +1,5 @@
 -- LOOP
 
--- Insert 10000 record vào bảng BoMon
--- Mà không trùng ID
--- Tên bộ môn tăng dần
 
 Use HowKteam
 Go
@@ -25,3 +22,27 @@ BEGIN
 END
 
 SELECT * from testCheck
+
+-- Insert 10000 record vào bảng BoMon
+-- Mà không trùng ID
+-- Tên bộ môn tăng dần
+
+DECLARE @i INT = 100
+DECLARE @n INT = 10100
+
+-- Điều kiện thực hiện 
+WHILE (@i < @n)
+BEGIN
+    -- Insert rows into table 'TableName' in schema '[dbo]'
+    INSERT INTO [dbo].[BOMON]
+    ( -- Columns to insert data into
+     [MABM], [TENBM], [PHONG],[DIENTHOAI],[TRUONGBM],[MAKHOA],[NGAYNHANCHUC]
+    )
+    VALUES
+    ( -- First row: values for the columns in the list above
+     @i, @i, 'A10','035 6401579', null, N'CNTT',GETDATE()
+    )
+    
+    SET @i += 1
+END
+SELECT * FROM BOMON
